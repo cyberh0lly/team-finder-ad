cdcdba48c3e084228f441a36bcdaaa8a8c211a43
cyberholly <cyberholly@mail.ru>
2026-09-19 13:40:59 +0300

    Initialize Django apps

--- team_finder/settings.py
+++ team_finder/settings.py
@@ -15,6 +15,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "users",
+    "projects",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
